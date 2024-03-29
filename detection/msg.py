ca35cacb002c8aedaa6a35ba462fb04b97eb09b4
cooper_W <junzhewang_2019@163.com>
2024-03-29 13:57:19 +0800

    common msg between py2 and py3

---
+++ detection/msg.py
@@ -0,0 +1,8 @@
+
+
+'''
+message type for detection
+msg = (type, data)
+message type:{'detect','event','program sign', 'speech' }
+data = {'detect':{true, false}, 'event':{red light, green light}, 'program sign':{start, stop}, 'speech':[speech1, speech2, speech3.....]}
+'''
